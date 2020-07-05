73b299882630139176d7638ffe95a8da9d0e08d5
halima1998 <halima19@navgurukul.org>
2020-07-05 09:45:39 +0530

    add env file

--- knexfile.js
+++ knexfile.js
@@ -9,11 +9,10 @@ module.exports = {
       user: process.env.DB_USER ,
       password:process.env.DB_PASS,
       host:process.env.DB_HOST
-      // port:process.env.APP_PORT
     },
     pool: {
-      min: 10,
-      max: 20
+      min: 2,
+      max: 10,
     },
     migrations: {
       tableName: 'knex_migrations',
